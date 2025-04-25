ba888a7e655598ea817256c38f1523fcc63e10c2
Ankush <deshwalankush23@gmail.com>
2025-04-26 02:37:20 +0530

    Project initiated

--- backend/expressjs-intro/index.js
+++ backend/expressjs-intro/index.js
@@ -2,15 +2,68 @@ const express = require('express');
 const PORT = 3000;
 
 const app = express();
+app.use(express.json());
+// app.use(express.static('./public'));
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
+// ---------
+function logger(req, res, next) {
+    console.log('Request URL:', req.url, "Reqesut body: ",  req.body);
+    next();
+}
+
+app.use(logger);
+// ---------
+
+app.get('/', function (req, res) {
+    res.json('Hello World!');
 })
 
-app.get('/ankush', function(req, res) {
+// HTTP : HYPER TEXT TRANSFER PROTOCOL
+// GET : GETTING DATA FROM SERVER
+// POST : SENDING DATA TO SERVER
+// PUT : UPDATING DATA ON SERVER
+// DELETE : DELETING DATA FROM SERVER
+// PATCH : UPDATING PARTIAL DATA ON SERVER
+
+app.post('/post', (req, res) => {
+    console.log(req.body);
+    res.json('Data received!', req.body.name);
+})
+
+app.put('/random', (req, res) => {
+    console.log(req.body);
+    res.json('Data received!', req.body.name);
+});
+app.delete('/delete', (req, res) => {
+    console.log(req.body);
+    res.json('Data received!', req.body.name);
+});
+
+app.patch('/patch', (req, res) => {
+    console.log(req.body);
+    res.json('Data received!', req.body.name);
+});
+
+app.post('/post', (req, res) => {
+    console.log(req.body);
+    res.json('Data received!', req.body.name);
+})
+
+app.get('/ankush', function (req, res) {
     res.send('Hello Ankush!');
 })
-    
-app.listen(PORT, function() {
+
+
+/*
+What is Middleware?
+Middleware in Express.js are functions that sit between
+the request (req) and the response (res). They can:
+
+- Modify the request or response.
+- End the request-response cycle.
+- Call the next middleware in the stack using next().
+*/
+
+app.listen(PORT, function () {
     console.log(`Server is running on port ${PORT}`);
 });
---
+++ food-felivery-app/backend/models/user.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    address: {
+        type: String,
+        required: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', userSchema);
